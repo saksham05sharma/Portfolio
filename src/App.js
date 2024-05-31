2641959beeae77ea2016f546bf810407beb09cd4
Saksham <saksham05052002@gmail.com>
2024-05-31 22:43:13 +0530

    adding projects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Main from './Components/Main/Main';
 import Navbar from './Components/Navbar/Navbar';
+import Projects from './Components/Projects/Projects';
 import BackgroundView from './layout/BackgroundView';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <div className='font-poppins select-none text-black bg-white dark:bg-[#20262E] dark:text-white  transition duration-500'>
           <Navbar />
           <Main />
+          <Projects />
         </div>
       </BackgroundView>
     </div>
---
+++ src/Data/ExperienceData/experienceData.js
@@ -0,0 +1,10 @@
+const ExperienceData = [
+    {
+        id: "1",
+        org: "Praedico Global Research Pvt. Ltd.",
+        title: "Full Stack Developer Intern",
+        date: "Jan 2023 - May 2023",
+        description:  "During my internship, I developed a user interface for ranking quarterly stock prices of companies across various sectors. This involved designing and implementing a component that allows users to subscribe to our website's services using the PayPal Subscriptions API, enabling seamless subscription management. Additionally, I played a key role in enhancing the technical documentation by creating and updating content to ensure clarity and accuracy for users and developers alike. Throughout the development process, I conducted thorough bug diagnosis and overhauls to ensure the smooth operation of our website and its features. These experiences greatly enriched my skills in frontend development, API integration, technical documentation, and debugging within a dynamic team environment.In this role, I design wireframes and create frontend pages from ideation to development. I blend creativity with technical expertise to deliver compelling user experiences. My focus spans from initial brainstorming sessions to the final polish of the user interface. I also collaborate with other team members to ensure seamless project execution."
+    }
+]
+export default {ExperienceData}
