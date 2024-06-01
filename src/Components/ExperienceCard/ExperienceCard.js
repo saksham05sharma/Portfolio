97be6212d6cced937b9602126ad927b0683aa078
Saksham <saksham05052002@gmail.com>
2024-06-01 13:56:18 +0530

    Experience added

--- src/Components/ExperienceCard/ExperienceCard.js
+++ src/Components/ExperienceCard/ExperienceCard.js
@@ -2,26 +2,26 @@ import React from 'react';
 import './ExperienceCard.css';
 const ExperienceCard = ({org, title, date, description}) => {
     return ( 
-        <div className='flex flex-col mt-10 items-center justify-center'>
-        <div className='flex flex-col sm:flex-row gap-10 ml-4 w-full'>
-          <div className='text-lg mb-2 ml-[-3px] mt-[0.1rem]'>
-            <h1 className='flex-1 min-w-[120px] sm:min-w-[92px]'>{org}</h1>
-            <h3 className='text-sm text-center my-1 dark:text-[#ccc]'>{date}</h3>
-          </div>
-          <div className='flex relative mt-[5px] flex-col sm:ml-[-29px]  '>
-            <p className="p-[1px] border-black dark:border-white rounded-50 bg-transparent ">
-              <div className='m-1 bg-[#ec6e59] p-[7px] rounded-full'></div>
-            </p>
-            <div className='absolute top-[29px] bottom-[-26px] left-[12px] sm:top-[26px] border-dotted border-l-2 border-black dark:border-white '></div>
-          </div>
-          <div className='flex flex-col mt-1'>
-            <h1 className='sm:w-[80%] text-lg mb-0 font-semibold '>{title}</h1>
-            <p className='sm:w-[80%] font-light text-custom-18 leading-loose text-sm sm:text-justify dark:text-[#ccc]'>
-              {description}
-            </p>
-          </div>
+        <div className="experience-container">
+            <div className="experience-content">
+                <div className="experience-org">
+                    <h1 className="org-name">{org}</h1>
+                    <h3 className="experience-date">{date}</h3>
+                </div>
+                <div className='experience-timeline'>
+                    <p className="timeline-dot">
+                        <div className='timeline-circle'></div>
+                    </p>
+                    <div className='timeline-line'></div>
+                </div>
+                <div className='experience-details'>
+                    <h1 className='experience-title'>{title}</h1>
+                    <p className='experience-description'>
+                        {description}
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
      );
 }
  
--- src/Components/Experiences/Experiences.js
+++ src/Components/Experiences/Experiences.js
@@ -5,25 +5,23 @@ import '../../App.css'
 import '../../index.css'
 const Experiences = () => {
     return ( 
-        <div id="experience" className='sm:pl-[8rem] p-8 mx-7 mt-4 sm:mt-0 sm:pt-16 pb-20 sm:px-4 sm:py-0'>
-      <div>
-        <h1 className='w-72 mb-8 tracking-wider font-semibold text-2xl sm:text-2xl md:text-2xl lg:text-2.5xl'>
-          Experience
-        </h1>
-      </div>
-
-      {
-        data.map((exp) => {
-          return (
-            <ExperienceCard
-              key={exp.id}
-              {...exp}
-            />
-          )
-        })
-      }
-       
-    </div>
+        <div id="experience" className="experience-container">
+            <div>
+                <h1 className="experience-title">
+                    Experience
+                </h1>
+            </div>
+            {
+                data.map((experience)=>{
+                    return (
+                        <ExperienceCard 
+                        key = {experience.id}
+                        {...experience}
+                        />
+                    )
+                })
+            }
+        </div>
      );
 }
  
